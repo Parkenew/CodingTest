563458dfc37b8a1d2ddea5cbc7b23f5cbe356794
ParkGeonWoo <126756270+Parkenew@users.noreply.github.com>
2024-10-01 18:56:19 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb=new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        st=new StringTokenizer(br.readLine());
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        sb.append(a+b).append("\n");
+        sb.append(a-b).append("\n");
+        sb.append(a*b).append("\n");
+        sb.append(a/b).append("\n");
+        sb.append(a%b);
+        System.out.println(sb);
+    }
+
+
+}
