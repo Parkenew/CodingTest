367dee522622ab72c64491201c83a6f9f7addafd
Geonwoo Park <126756270+Parkenew@users.noreply.github.com>
2026-01-20 23:28:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 600 ms, Memory: 221340 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        static StringBuilder sb = new StringBuilder();
+        static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        static StringTokenizer st;
+        static int N;
+        public static void main(String[] args) throws IOException {
+                N = Integer.parseInt(br.readLine());
+                for (int i = 0; i < N; i++) {
+                        st=new StringTokenizer(br.readLine());
+                        int a = Integer.parseInt(st.nextToken());
+                        int b = Integer.parseInt(st.nextToken());
+                        sb.append(a+b).append("\n");
+                }
+                System.out.println(sb);
+
+        }
+
+    }
+
