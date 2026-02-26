6519e2565911edd0587569242edb60a9ef228ccc
Geonwoo Park <126756270+Parkenew@users.noreply.github.com>
2026-02-26 21:04:47 +0900

    [Bronze V] Title: 나머지, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int A,B,C;
+    public static void main(String[] args) throws IOException {
+        st=new StringTokenizer(br.readLine());
+        A=Integer.parseInt(st.nextToken());
+        B=Integer.parseInt(st.nextToken());
+        C=Integer.parseInt(st.nextToken());
+        
+        sb.append((A+B)%C).append("\n");
+        sb.append(((A%C) + (B%C))%C).append("\n");
+        sb.append((A*B)%C).append("\n");
+        sb.append(((A%C) * (B%C))%C);
+        System.out.println(sb);
+    }
+
+}
